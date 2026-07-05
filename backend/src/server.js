887f0a1cc3a13b7f5e19510ba567449379b06019
Anya2706 <anya.rajan.24cse@bmu.edu.in>
2026-07-05 22:57:51 +0530

    Complete P4 - Rooms & Bookings modules with filtering, booking creation, and user bookings

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bookingRoutes = require('./routes/bookingRoutes');
 require('dotenv').config();
 
 const authRoutes          = require('./routes/authRoutes');
@@ -16,7 +17,7 @@ app.use('/api/auth',          authRoutes);
 app.use('/api/dashboard',     dashboardRoutes);
 app.use('/api/organizations', organizationRoutes);
 app.use('/api/rooms', roomRoutes);
-
+app.use('/api/bookings', bookingRoutes);
 app.get('/api/test', (req, res) => {
     res.json({ message: 'StockNest Backend is Running!' });
 });
